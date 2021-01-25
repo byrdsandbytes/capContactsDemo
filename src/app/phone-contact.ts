73278cda1264c97b6f2100dd6dd8a1e42727b176
Idrimi <gerber@byrds.ch>
2021-01-25 22:24:41 +0100

    updated to to new cap contacts version

--- src/app/phone-contact.ts
+++ src/app/phone-contact.ts
@@ -1,7 +1,24 @@
-export interface PhoneContact {
+export interface PhoneNumber {
+  label?: string;
+  number?: string;
+}
+
+export interface EmailAddress {
+  label?: string;
+  address?: string;
+}
+
+export interface Contact {
   contactId: string;
-  lookupKey: string;
-  displayName: string;
-  phoneNumbers: [string];
-  emails: [string];
+  displayName?: string;
+  phoneNumbers: PhoneNumber[];
+  emails: EmailAddress[];
+  organizationName?: string;
+  organizationRole?: string;
+  birthday?: string;
+}
+
+export interface ContactsPlugin {
+  getPermissions(): Promise<PermissionStatus>;
+  getContacts(): Promise<{ contacts: Contact[] }>;
 }
